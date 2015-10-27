62a55c9e4c46aac647e0c7bc3d8143f1a6bd41ca
Adam Thomas <sortoflikechess@gmail.com>
2015-10-27 11:54:43 +0000

    Use filter_horizontal for many-to-many fields.

--- groups/admin.py
+++ groups/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 from .models import Discussion, Group
 
 
-admin.site.register(Discussion)
-admin.site.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    filter_horizontal = ('moderators', 'watchers', 'members_if_private')
+
+    class Meta:
+        model = Group
+
+
+class DiscussionAdmin(admin.ModelAdmin):
+    filter_horizontal = ('subscribers', 'ignorers')
+
+    class Meta:
+        model = Discussion
+
+
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Discussion, DiscussionAdmin)
