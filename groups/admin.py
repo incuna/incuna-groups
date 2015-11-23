7619142e3716221df52061e8d9deca6e96537f52
Adam Thomas <sortoflikechess@gmail.com>
2015-11-23 16:25:24 +0000

    [ci skip] Moar documentation.

--- groups/admin.py
+++ groups/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
 
+# Registered in the `AppConfig`
 class GroupAdmin(admin.ModelAdmin):
     filter_horizontal = ('moderators', 'members_if_private')
     exclude = ('watchers',)
 
 
+# Also registered in the `AppConfig`
 class DiscussionAdmin(admin.ModelAdmin):
     exclude = ('subscribers', 'ignorers')
