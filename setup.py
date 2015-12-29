d09162c4a0386e32c85fb51adea43972b3191e07
Adam Thomas <sortoflikechess@gmail.com>
2015-12-29 16:08:17 +0000

    Avoid broken django-polymorphic versions.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'django_crispy_forms>=1.4.0,<2',
-        'django-polymorphic>=0.7.2,<1',
+        'django-polymorphic>=0.7.2,<0.8',
         'incuna-pagination>=0.1.1,<1',
     ],
     description='Generic group/forum framework.',
