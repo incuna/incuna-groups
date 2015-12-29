0fcc0e44a7a11f80b4fd6ec76455c8a744f4af0d
Adam Thomas <sortoflikechess@gmail.com>
2015-12-29 17:18:24 +0000

    Prettify the labels on the new upload form.

--- groups/forms.py
+++ groups/forms.py
@@ -57,6 +57,12 @@ class AddFileComment(BaseAddCommentForm):
 class AddTextCommentWithAttachment(BaseAddCommentForm):
     file = forms.FileField()
 
+    def __init__(self, *args, **kwargs):
+        """Add nicer field labels."""
+        super(AddTextCommentWithAttachment, self).__init__(*args, **kwargs)
+        self.fields['body'].label = 'Comment'
+        self.fields['file'].label = 'Attachment'
+
     def build_helper(self):
         helper = super(AddTextCommentWithAttachment, self).build_helper()
         helper.layout = Layout(
